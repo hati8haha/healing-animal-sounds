4c10a06abf1046f2f365d781cb87005b5efdfd4e
haoting <haoting3462@gmail.com>
2023-06-07 17:48:27 +0800

    feat: folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:english_words/english_words.dart';
+import 'package:first_app/pages/playing.dart';
+import 'package:first_app/pages/setting.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import './pages/landing.dart';
+import './pages/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -62,18 +66,19 @@ class _MyHomePageState extends State<MyHomePage> {
     Widget page;
     switch (selectedIndex) {
       case 0:
-        page = GeneratorPage();
+        page = LandingPage();
         break;
       case 1:
-        page = FavoritesPage();
+        page = HomePage();
         break;
       case 2:
-        page = CustomGridView();
+        page = PlayingPage();
+        break;
+      case 3:
+        page = SettingPage();
         break;
       default:
-        page = GeneratorPage();
-
-      // throw UnimplementedError('no widget for $selectedIndex');
+        page = HomePage();
     }
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
@@ -106,31 +111,6 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: Row(
           children: [
-            SafeArea(
-              child: NavigationRail(
-                extended: constraints.maxWidth >= 600,
-                destinations: [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.home),
-                    label: Text('Home'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.favorite),
-                    label: Text('Favorites'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.favorite),
-                    label: Text('聲音'),
-                  ),
-                ],
-                selectedIndex: selectedIndex,
-                onDestinationSelected: (value) {
-                  setState(() {
-                    selectedIndex = value;
-                  });
-                },
-              ),
-            ),
             Expanded(
               child: Container(
                 color: Theme.of(context).colorScheme.primaryContainer,
@@ -143,151 +123,3 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 }
-
-class FavoritesPage extends StatelessWidget {
-  const FavoritesPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: appState.clearFavorites, child: Text('clear all')),
-          ],
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
-    );
-  }
-}
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!
-        .copyWith(color: theme.colorScheme.onPrimary);
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: "${pair.first} ${pair.second}",
-        ),
-      ),
-    );
-  }
-}
-
-class CustomGridView extends StatelessWidget {
-  // final data = List.generate(12, (i) => Color(0xFFFF00FF - 2 * i));
-  final data = List.generate(12, (i) => Color(0xff00ffff - 2 * i));
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: GridView.count(
-        crossAxisCount: 2,
-        mainAxisSpacing: 12,
-        crossAxisSpacing: 20,
-        childAspectRatio: 1 / 0.5,
-        children: data.map((color) => _buildItem(color)).toList(),
-      ),
-    );
-  }
-
-  Container _buildItem(Color color) => Container(
-        alignment: Alignment.center,
-        width: 100,
-        height: 30,
-        color: color,
-        child: Text(
-          colorString(color),
-          style: TextStyle(color: Colors.white, shadows: [
-            Shadow(color: Colors.black, offset: Offset(.5, .5), blurRadius: 2)
-          ]),
-        ),
-      );
-  
-  String colorString(Color color) =>
-      "#${color.value.toRadixString(16).padLeft(8, '0').toUpperCase()}";
-}
-
-
