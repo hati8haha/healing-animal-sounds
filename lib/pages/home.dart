ea0b04f0a152411a61396d3f815551ed5aa13aa5
Haoting Cheng <haoting3462@gmail.com>
2023-06-09 17:16:14 +0800

    feat: background image and landpage layout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,42 +1,28 @@
-import 'package:healing_animal_sounds/main.dart';
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({
-    super.key,
-  });
-
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: appState.clearFavorites, child: Text('clear all')),
-          ],
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
+    return DecoratedBox(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background/grass-sea.png'),
+              fit: BoxFit.cover)),
+      child: Center(
+        child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+            child: (Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('App name'),
+                Image(
+                    image: AssetImage('assets/images/animals/bird.png'),
+                    fit: BoxFit.fitHeight),
+              ],
+            ))),
+      ),
     );
   }
 }
--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -1,49 +1,21 @@
-import 'package:healing_animal_sounds/main.dart';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../widgets/big_card.dart';
 
 class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-              Image(
-                  image: AssetImage('assets/images/background/golden-sea.png'))
-            ],
-          ),
-        ],
+    return DecoratedBox(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background/grass-sea.png'),
+              fit: BoxFit.cover)),
+      child: Center(
+        child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+            child: (Image(
+                image: AssetImage('assets/images/animals/bird.png'),
+                fit: BoxFit.fitHeight))),
       ),
     );
   }
