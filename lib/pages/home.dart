7243e597578bdb13f8313b218c7e29c466602ed2
jeremy <jeremywang077@gmail.com>
2023-06-14 19:56:49 +0800

    home page v1

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,27 +1,143 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> buttonLabels = [
+    '全部',
+    '森林',
+    '地獄生物',
+    '吉娃娃',
+    '蘿莉',
+    '正太',
+    '御姊',
+    '萌妹'
+  ];
+  int selectedIndex = 0;
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void scrollToSelectedButton() {
+    if (selectedIndex > 0) {
+      const double buttonWidth = 100.0;
+      final double scrollOffset =
+          (selectedIndex * buttonWidth) - (2 * buttonWidth);
+      final double maxScrollExtent = _scrollController.position.maxScrollExtent;
+
+      if (scrollOffset < maxScrollExtent) {
+        _scrollController.animateTo(
+          scrollOffset,
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+        );
+      } else {
+        _scrollController.animateTo(
+          maxScrollExtent,
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+        );
+      }
+    } else {
+      _scrollController.animateTo(
+        0.0,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/background/grass-sea.png'),
-              fit: BoxFit.cover)),
-      child: Center(
-        child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-            child: (Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text('App name'),
-                Image(
-                    image: AssetImage('assets/images/animals/bird.png'),
-                    fit: BoxFit.fitHeight),
-              ],
-            ))),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.blue[300],
+          title: Text('動物白噪音'),
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                // Add your onPressed logic here
+              },
+            ),
+          ],
+        ),
+        body: Container(
+          padding: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background/home-bg.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10.0),
+              SizedBox(
+                width: double.infinity,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  controller: _scrollController,
+                  child: Row(
+                    children: [
+                      for (int index = 0; index < buttonLabels.length; index++)
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5.0),
+                          child: ChoiceChip(
+                            backgroundColor: Colors.white,
+                            label: Text(buttonLabels[index]),
+                            selected: selectedIndex == index,
+                            selectedColor: Colors.blue,
+                            onSelected: (bool selected) {
+                              setState(() {
+                                selectedIndex = selected ? index : -1;
+                              });
+                              scrollToSelectedButton();
+                            },
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    children: List.generate(8, (index) {
+                      return Center(
+                        child: Image.asset(
+                          'assets/images/animals/bird.png', // Replace with your image asset path
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Image(image: AssetImage("assets/images/others/vip.png")),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/pages/home_test.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final List<String> tabs = ['全部', '森林', '地獄生物', '吉娃娃'];
+  @override
+  Widget build(BuildContext context) {
+    const title = '動物白噪音';
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      home: DefaultTabController(
+        length: tabs.length,
+        child: Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(60),
+            child: AppBar(
+              // backgroundColor: Colors.transparent,
+              elevation: 0,
+              title: Text(
+                title,
+                style: TextStyle(color: Colors.black),
+              ),
+              actions: [
+                IconButton(
+                  icon: Icon(
+                    Icons.menu,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    // Add your onPressed logic here
+                  },
+                ),
+              ],
+              bottom: TabBar(
+                tabs: tabs
+                    .map((tab) => Tab(
+                          child: Text(
+                            tab,
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ))
+                    .toList(),
+              ),
+            ),
+          ),
+          body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background/home-bg.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: TabBarView(
+              children: tabs.map((tab) {
+                return GridView.count(
+                  crossAxisCount: 2,
+                  children: List.generate(8, (index) {
+                    return Center(
+                      child: Text(
+                        'Item $index',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    );
+                  }),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// child: GridView.count(
+//               crossAxisCount: 2,
+//               children: List.generate(8, (index) {
+//                 return Center(
+//                   child: Text(
+//                     'Item $index',
+//                     style: Theme.of(context).textTheme.headlineSmall,
+//                   ),
+//                 );
+//               }),
