4b1da22bb81a992ecbf66276424eac32c6446dc0
HaoTing Cheng <haoting3462@gmail.com>
2023-11-08 01:18:35 +0800

    feat: add riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:json_theme/json_theme.dart';
 import 'dart:convert';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'utils/theme_provider.dart';
 import 'constants/theme.dart';
 import './pages/home.dart';
 import './pages/landing.dart';
@@ -16,16 +18,17 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var darkMode = ref.watch(darkModeProvider);
     final themeString = ThemeString();
     final themeLightStr = themeString.themeLight;
     final themeLightJson = jsonDecode(themeLightStr);
@@ -40,6 +43,7 @@ class MyApp extends StatelessWidget {
       title: '會動的療癒夥伴－動物叫聲、白噪音、放鬆、睡眠、紓壓',
       theme: themeLight,
       darkTheme: themeDark,
+      themeMode: darkMode ? ThemeMode.dark : ThemeMode.light,
       initialRoute: '/landing',
       routes: {
         '/landing': (context) => LandingPage(),
